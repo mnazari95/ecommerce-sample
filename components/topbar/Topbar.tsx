accdbb25e34e1b07ab55db3540a4fa37c7230fd2
HafizN <hafizn2013@outlook.com>
2023-01-01 20:30:12 -0800

    added css banner, topbar component

--- components/topbar/Topbar.tsx
+++ components/topbar/Topbar.tsx
@@ -1,10 +1,31 @@
+import Link from 'next/link'
 import styles from '../../styles/Topbar.module.css'
+import { useState } from 'react';
 
 export default function Topbar() {
+
+  const [menuToggle, setMenuToggle] = useState(false);
+
+  function toggleMenu() {
+    //toggles side menu when activated
+    return(
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/deal">Deals</Link>
+        </li>
+        <li>
+          <Link href="/all">All</Link>
+        </li>
+      </ul>
+      )
+  }
   return (
     <div className={styles.container}>
       <ul className={styles.listContainer}>
-        <li className={styles.list}>Menu</li>
+        <li className={styles.list} onClick={() => setMenuToggle(true)}>Menu</li>
         <li className={styles.list}>Profile</li>
       </ul>
       <ul className={styles.listContainer}>
