3796dadcd1797e88badf7e5881f0d5baa48de365
HafizN <hafizn2013@outlook.com>
2023-05-18 21:25:52 -0700

    playing around with copilot

--- components/banner/Banner.tsx
+++ components/banner/Banner.tsx
@@ -1,10 +1,64 @@
 import styles from '../../styles/Banner.module.css'
 import Image from 'next/image'
+import { useState } from 'react'
+
+//create a banner component with the functionality of a slideshow
+
 
 export default function Banner() {
+
+  //create a slideshow of images
+  const [slideIndex, setSlideIndex] = useState(0)
+  const [slideImages, setSlideImages] = useState([
+    '/banner-image.jpg',
+    '/banner-image.jpg',
+    '/banner-image.jpg',
+    '/banner-image.jpg',
+    '/banner-image.jpg',
+    '/banner-image.jpg',
+    '/banner-image.jpg',
+    '/banner-image.jpg',
+    '/banner-image.jpg',
+    '/banner-image.jpg',
+    '/banner-image.jpg',
+    '/banner-image.jpg',
+    '/banner-image.jpg',
+    '/banner-image.jpg',
+    '/banner-image.jpg',
+  ])
+
+  const nextSlide = () => {
+    if (slideIndex !== slideImages.length - 1) {
+      setSlideIndex(slideIndex + 1)
+    } else if (slideIndex === slideImages.length - 1) {
+      setSlideIndex(0)
+    }
+  }
+
+  const prevSlide = () => {
+    if (slideIndex !== 0) {
+      setSlideIndex(slideIndex - 1)
+    } else if (slideIndex === 0) {
+      setSlideIndex(slideImages.length - 1)
+    }
+  }
+
+  const moveDot = (index) => {
+    setSlideIndex(index)
+  }
+
   return (
     <div className={styles.bannerImage}>
-      <Image src="/banner-image.jpg" alt="nice background picture" width={0} height={0} sizes='100vw' style={{width: '100%', height: 'auto', objectFit: 'contain', }}/>
+      {
+        slideImages.map((img, index) => {
+          return (
+            <div key={index}>
+              <Image src={img} alt="nice background picture" width={0} height={0} sizes='100vw' style={{width: '100%', height: 'auto', objectFit: 'contain', }}/>
+            </div>
+          )
+        })
+      }
+      <button className={styles.prev} onClick={prevSlide}>Prev</button>
     </div>
   )
 }
--- components/filteredCatalog/FilterOptions.tsx
+++ components/filteredCatalog/FilterOptions.tsx
@@ -1,17 +1,33 @@
 import styles from '../../styles/FilterOptions.module.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function FilterOptions() {
 
-    // const [imgSize, setImgSize] = useState({
-    //     width: 230,
-    //     height: 296
-    // })
+  const [fruitFilter, setFruitFilter] = useState(['apple', 'banana', 'cherry', 'durian', 'elderberry', 'fig', 'grape', 'honeydew', 'jackfruit', 'kiwi', 'lemon', 'mango', 'nectarine'])
+  
+  const filterByFruit = (fruit) => {
+    setFruitFilter(fruitFilter.filter((f) => f !== fruit))
+  }
 
+  useEffect(() => { 
+    console.log(fruitFilter)
+  }, [fruitFilter])
+
+  const fruitList = fruitFilter.map((fruit) => {
+    return (
+      <div key={fruit} className={styles.fruit}>
+        <span>{fruit}
+        <button className={styles.removeCheckBoxBtn} onClick={() => filterByFruit(fruit)}>Remove</button></span>
+        <br />
+      </div>
+    )
+  })
   return (
     <div className={styles.container}>
-        <p>Filter options</p>
-        <button>Apply</button>
+        <h3>Filter by:</h3>
+        <h4>Category</h4>
+        {fruitList}
+        <button className={styles.filterBtn}>Apply</button>
     </div>
   )
 }
